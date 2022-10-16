546e84aa89c9c3091300925bc256fbce7881cf26
Anthony Salas <anthony.salas@tecsup.edu.pe>
2022-10-15 23:04:17 -0500

    Finalizado

--- OrderController.cs
+++ OrderController.cs
@@ -5,11 +5,11 @@ namespace BlazingPizza;
 
 [Route("orders")]
 [ApiController]
-public class OrdersController : Controller
+public class OrderController : Controller
 {
     private readonly PizzaStoreContext _db;
 
-    public OrdersController(PizzaStoreContext db)
+    public OrderController(PizzaStoreContext db)
     {
         _db = db;
     }
@@ -44,5 +44,21 @@ public class OrdersController : Controller
         await _db.SaveChangesAsync();
 
         return order.OrderId;
+    }
+    [HttpGet("{orderId}")]
+    public async Task<ActionResult<OrderWithStatus>> GetOrderWithStatus(int orderId)
+    {
+        var order = await _db.Orders
+            .Where(o => o.OrderId == orderId)
+            .Include(o => o.Pizzas).ThenInclude(p => p.Special)
+            .Include(o => o.Pizzas).ThenInclude(p => p.Toppings).ThenInclude(t => t.Topping)
+            .SingleOrDefaultAsync();
+
+        if (order == null)
+        {
+            return NotFound();
+        }
+
+        return OrderWithStatus.FromOrder(order);
     }
 }
